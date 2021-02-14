33a6fc5744920a0fb5ab1bd8af607f0f5c7a3429
Andrey Spiridonov <spiridonov.a@future-group.ru>
2021-02-14 16:06:41 +0300

    clean card component

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -19,33 +19,41 @@ const Card: React.FC<ICardProps> = ({ dog }) => {
   const { state, dispatch } = React.useContext(FavoritesContext) as any;
   const isFavorites = Object.keys(state).includes(dog.id);
 
-  const BORDER_SIZE = isFavorites ? 5 : 0;
+  const BORDER_SIZE = React.useMemo(() => (isFavorites ? 5 : 0), [isFavorites]);
+  const onLongPress = React.useCallback(() => {
+    dispatch(
+      !isFavorites ? addDogToFavorites(dog) : removeDogFromFavorites(dog.id)
+    );
+  }, [dispatch, dog, isFavorites]);
+
+  const ImageStyles = React.useMemo(
+    () => ({
+      width: IMAGE_SIZE - BORDER_SIZE * 2,
+      height: IMAGE_SIZE - BORDER_SIZE * 2,
+    }),
+    [isFavorites]
+  );
 
   return (
     <RN.Pressable
-      style={
-        isFavorites && {
-          borderWidth: BORDER_SIZE,
-          width: IMAGE_SIZE,
-          height: IMAGE_SIZE,
-          borderColor: '#0000ff',
-        }
-      }
-      onLongPress={() =>
-        dispatch(
-          !isFavorites ? addDogToFavorites(dog) : removeDogFromFavorites(dog.id)
-        )
-      }
+      style={[s.favorite, isFavorites && s.border]}
+      onLongPress={onLongPress}
     >
-      <Image
-        style={{
-          width: IMAGE_SIZE - BORDER_SIZE * 2,
-          height: IMAGE_SIZE - BORDER_SIZE * 2,
-        }}
-        uri={dog.imagePath}
-      />
+      <Image style={ImageStyles} uri={dog.imagePath} />
     </RN.Pressable>
   );
 };
+
+const s = RN.StyleSheet.create({
+  favorite: {
+    width: IMAGE_SIZE,
+    height: IMAGE_SIZE,
+    borderColor: '#0000ff',
+  },
+
+  border: {
+    borderWidth: 5,
+  },
+});
 
 export default Card;
